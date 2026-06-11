c15aa71a47ec53b960ce4ed78de53a3f509521b4
Utkarsh Gupta <utkarshg390@gmail.com>
2026-06-11 22:01:56 +0530

    Add files via upload

--- Day9/C/Ques34.c
+++ Day9/C/Ques34.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
     for(int i=5;i>0;i--){
-        for(int j=0;j<i;j++){
-            printf(" *");
+        for(int j=1;j<=i;j++){
+            printf(" %d",j);
         }
         printf("\n");
     }
